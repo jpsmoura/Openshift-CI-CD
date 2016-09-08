cec9056dee97604b316c402a54826019af8f8ae8
jpsmoura <joaomouraemail@gmail.com>
2016-09-08 15:00:03 +0100

    Process command updated

--- scripts/create-project.sh
+++ scripts/create-project.sh
@@ -7,4 +7,4 @@ oc adm policy add-role-to-user edit -z jenkins -n ${PROJECT_NAME}
 OSE_TOKEN=$(oc get sa/jenkins -n ${PROJECT_NAME} --template='{{range .secrets}}{{ .name }} {{end}}' | xargs -n 1 oc get secret --template='{{ if .data.token }}{{ .data.token }}{{end}}' | head -n 1 | base64 -D -)
 echo "OSE_TOKEN=$OSE_TOKEN" > env_start.txt
 
-oc process -f ../templates/dev-contacts.json | oc create -n ${PROJECT_NAME} -f - 
+oc process -f templates/build-template.json -v GIT_REPO=https://github.com/jpsmoura/Openshift-CI-CD.git | oc create -n ${PROJECT_NAME} -f - 
